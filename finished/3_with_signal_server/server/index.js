def77084d93a779f814a57151ad941f62a787cf5
Chigozie Oduah <87097037+GhoulKingR@users.noreply.github.com>
2024-03-17 09:40:18 +0100

    optimized codebase

--- finished/3_with_signal_server/server/index.js
+++ finished/3_with_signal_server/server/index.js
@@ -1,18 +1,20 @@
 const { WebSocketServer } = require("ws");
+
+const channels = {};
 const server = new WebSocketServer({ port: 80 });
 server.on("connection", handleConnection);
 
-const channels = {};
-
 function handleConnection(ws) {
   console.log('New connection');
-  ws.send(JSON.stringify({type: 'connection-established'}));
-
+  ws.send( JSON.stringify({ type: 'connection-established' }) );
+  
   let id;
   let channel = "";
 
+  ws.on("error", () => console.log('websocket error'));
   ws.on('message', message => {
     const object = JSON.parse(message);
+    
     if (object.type === "join-channel") {
       channel = object.channel;
       if (channels[channel] === undefined) channels[channel] = [];
@@ -20,14 +22,12 @@ function handleConnection(ws) {
       channels[channel].push(ws);
       ws.send(JSON.stringify({type: 'joined-channel', channel}));
     } else {
+      // forward the message to other channel memebers
       channels[channel]?.filter((_, i) => i !== id).forEach((member) => {
-        // all channel memebers except `ws`
         member.send(message.toString());
       });
     }
   });
-
-  ws.on("error", () => console.log('websocket error'));
   ws.on('close', () => {
     console.log('Client has disconnected!');
     if (channel !== "") channels[channel] = channels[channel].filter((_, i) => i !== id);
--- finished/3_with_signal_server/signalobject.js
+++ finished/3_with_signal_server/signalobject.js
@@ -1,18 +1,15 @@
 class SignalServer {
-  ready = false;
-
   constructor(channel) {    
     this.socket = new WebSocket("ws://localhost:80");
 
     this.socket.addEventListener("open", () => {
-      this.postMessage({type: "join-channel", channel});
-      this.ready == true;
+      this.postMessage({ type: "join-channel", channel });
     });
 
     this.socket.addEventListener("message", (e) => {
       const object = JSON.parse(e.data);
-      if (object.type === "joined-channel") console.log("Joined channel: " + object.channel);
-      else if (object.type === "connection-established") console.log("connection established");
+      if (object.type === "connection-established") console.log("connection established");
+      else if (object.type === "joined-channel") console.log("Joined channel: " + object.channel);
       else this.onmessage({ data: object });
     });
   }
@@ -20,7 +17,6 @@ class SignalServer {
   onmessage(e) {}
 
   postMessage(data) {
-    const jsonString = JSON.stringify(data);
-    this.socket.send(jsonString);
+    this.socket.send( JSON.stringify(data) );
   }
 }
